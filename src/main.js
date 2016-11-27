21414d3b83563c911f1dc1737a543eccec03b298
Juan José Franco <juanjo@juanjofp.com>
2016-11-27 11:36:28 +0100

    Add comments to client object

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import ClientLib from './libs/client';
 
 export const Client = ClientLib;
-
+/*
+{
+    protocol = 'ws',
+    server = 'localhost',
+    port = '8080',
+    onData,
+    onError
+}
+devuelve la función sendAction(service, room, action)
+service = 'DEFAULT', // Servicio al que comunicar
+room = 'DEFAULT', // Room con la que comunicar
+action = {} // Accion a enviar
+*/
 export default Client;
